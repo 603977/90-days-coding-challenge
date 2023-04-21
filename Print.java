52e64b34131bb899c50baa9a0a21a15cec60e8d4
Priyanka Pandey <110595977+603977@users.noreply.github.com>
2023-04-21 22:29:17 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,17 @@
+//Program to display no from 1 to 10
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,n;
+		System.out.println("No from 1 to 10 are as follows: ");
+		for(i=1;i<=10;i++)
+	    {
+	        System.out.println(+i);
+	        
+	    }
+	        
+
+	}
+
+}
