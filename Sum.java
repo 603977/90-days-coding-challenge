4a35912c72361937e5dc55eaf20031bb4d91234d
Priyanka Pandey <110595977+603977@users.noreply.github.com>
2023-04-21 22:33:33 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+//Program to display sum of no from 1 to 10
+public class Sum
+{  
+public static void main(String[] args)   
+{  
+int i,sum=0,num=10; 
+for(i = 1; i <= num; ++i)  
+{  
+sum = sum + i;  
+}  
+System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+}  
+}  
