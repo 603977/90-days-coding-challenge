98d1f1712d0aa293c7eb2bafb2a734de61e48355
Priyanka Pandey <110595977+603977@users.noreply.github.com>
2023-04-22 22:55:11 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Table 
+{
+public static void main(String[] args) 
+{
+int num;
+Scanner sc = new Scanner(System.in);
+ num1 = sc.nextInt();
+  System.out.println("Enter a number to get the multiplication: ");
+ 
+  for (int i=0; i< 10; i++)
+  {
+   System.out.println(num1 + " x " + (i+1) + " = " + (num1 * (i+1)));
+  }
+ }
+}
